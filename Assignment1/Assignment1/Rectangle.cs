c5e88a18170e1554d91ff5a0b6636ac3adc1d30e
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-02-06 09:47:20 -0500

    Rectangle class completed

---
+++ Assignment1/Assignment1/Rectangle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Rectangle
+    {
+        //variables to store attributes of a rectangle
+        private int length;
+        private int width;
+
+        //default constrcutor
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+
+        //parametarised constructor
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        
+        //getters
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+        public int GetWidth()
+        {
+            return this.width;
+        }
+
+        //setters
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        
+        //member functions
+
+        //get the perimeter of the rectangle
+        public int GetPerimeter()
+        {
+            return 2 * (this.length + this.width);
+        }
+
+        //get the area of the retangle
+        public int GetArea()
+        {
+            return this.length * this.width;
+        }
+    }
+}
