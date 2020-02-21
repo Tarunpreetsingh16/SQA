39c9fff8dca0b011500af4dc111cddf570978864
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-02-21 01:30:58 -0500

    Added Analyze and all checks, Testing left

--- Assignment2/Assignment02/Assignment02/Program.cs
+++ Assignment2/Assignment02/Assignment02/Program.cs
@@ -8,18 +8,40 @@ namespace Assignment02
 {
     class Program
     {
+        static string[] menu = { "Enter triangle dimensions",
+                              "Exit" };
         static void Main(string[] args)
         {
-            //show the menu and get the user input
-            int input = GetUserInput();
-            Console.ReadKey();
+            do {
+                //show the menu and get the user input
+                int input = GetUserInput();
+                if (input == menu.Length)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    //get the dimensions of the triangle
+                    int[] dimensions = GetDimensions();
+                    string result = TriangleSolver.Analyze(dimensions[0], dimensions[1], dimensions[2]);
+                    Console.Write("\n\n" + result);
+                }
+            } while (true);
+        }
+
+        private static int[] GetDimensions()
+        {
+            Console.Write("\n\n=====Give dimensions of a triangle=====");
+            int sideA = GetTriangleSide('A');
+            int sideB = GetTriangleSide('B');
+            int sideC = GetTriangleSide('C');
+            //return the dimensions of the triangle
+            return new int[] { sideA, sideB, sideC };
         }
 
         static int GetUserInput()
         {
             int input;
-            string[] menu = { "Enter triangle dimensions",
-                              "Exit" };
             do
             {
                 try
@@ -52,6 +74,27 @@ namespace Assignment02
             {
                 Console.Write("\n" + (i+1) + ". " + menuOptions[i]);
             }
+        }
+
+        static int GetTriangleSide(char sideChar)
+        {
+            int sideDimension;
+            do
+            {
+                try
+                {
+                    //get triangle dimension
+                    Console.Write("\n\nSide {0}: ", sideChar);
+                    sideDimension = Int32.Parse(Console.ReadLine());
+                    break; //break when input is invalid
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Please enter correct value");
+                }
+            }
+            while (true);
+            return sideDimension;
         }
         
     }
---
+++ Assignment2/Assignment02/Assignment02/TriangleSolver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    static class TriangleSolver
+    {
+        public static string Analyze(int sideA, int sideB, int sideC)
+        {
+            string output = String.Empty;
+            /*
+             * triangle is valis when
+             * sideA + sideB > sideC
+             * sideA + sideC > sideB
+             * sideB + sideC > sideA
+            */
+            if((sideA + sideB > sideC) &&
+               (sideA + sideC > sideB) &&
+               (sideB + sideC > sideA))
+            {
+                //equilateral check -> all sides are equal
+                if(sideA == sideB && 
+                   sideB == sideC)
+                {
+                    output = "Equilateral Triangle";
+                }
+                //scalene triangle check -> all sides are different
+                else if(sideA != sideB && 
+                        sideA != sideC &&
+                        sideB != sideC)
+                {
+                    output = "Scalene Triangle";
+                }
+                //isosceles triangle check -> two of the sides are same
+                else if (sideA == sideB || 
+                        sideA == sideC ||
+                        sideB == sideC)
+                {
+                    output = "Isosceles Triangle";
+                }
+            }
+            else
+            {
+                output = "Dimensions don't compose a triangle";
+            }
+            return output;
+        }
+    }
+}
