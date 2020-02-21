72487733e8dfccf3533f9a15bb5eba403e38cdb2
Tarunpreet Singh <tarunpreetsingh16@gmail.com>
2020-02-21 00:06:22 -0500

    Initialized project

---
+++ Assignment2/Assignment02/Assignment02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menu = { "Enter triangle dimensions",
+                              "Exit" };
+            ShowMenu(menu);
+        }
+
+        //show menu to the user
+        static void ShowMenu(string[] menuOptions) 
+        {
+            for (int i = 0;i < menuOptions.Length;i++)
+            {
+                Console.Write("\n" + i+1 + ". " + menuOptions[i]);
+            }
+        }
+    }
+}
